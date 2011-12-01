7223a96ffdf2dee8827829bf698ba696909d2491
Stephan Wahlbrink <sw@wahlbrink.eu>
2011-12-01 15:14:25 +0100

    [LTK-Model] Added abstract model manager
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

---
+++ de.walware.ecommons.ltk.core/src/de/walware/ecommons/ltk/core/impl/AbstractModelEventJob.java
@@ -0,0 +1,186 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2011 WalWare/StatET-Project (www.walware.de/goto/statet).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Stephan Wahlbrink - initial API and implementation
+ *******************************************************************************/
+
+package de.walware.ecommons.ltk.core.impl;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.ISafeRunnable;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.SafeRunner;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+
+import de.walware.ecommons.ltk.ElementChangedEvent;
+import de.walware.ecommons.ltk.IElementChangedListener;
+import de.walware.ecommons.ltk.IModelElement;
+import de.walware.ecommons.ltk.IModelElementDelta;
+import de.walware.ecommons.ltk.ISourceUnit;
+import de.walware.ecommons.ltk.ISourceUnitModelInfo;
+import de.walware.ecommons.ltk.LTK;
+import de.walware.ecommons.ltk.LTKUtil;
+import de.walware.ecommons.ltk.WorkingContext;
+import de.walware.ecommons.ltk.internal.core.LTKCorePlugin;
+
+
+/**
+ * Abstract model update event job
+ */
+public abstract class AbstractModelEventJob<ElementType extends IModelElement, InfoType extends ISourceUnitModelInfo> extends Job {
+	
+	
+	private static class SafeRunnable implements ISafeRunnable {
+		
+		final ElementChangedEvent fEvent;
+		IElementChangedListener fListener;
+		
+		public SafeRunnable(final ElementChangedEvent event) {
+			fEvent = event;
+		}
+		
+		@Override
+		public void run() {
+			fListener.elementChanged(fEvent);
+		}
+		
+		@Override
+		public void handleException(final Throwable exception) {
+			LTKCorePlugin.getDefault().log(new Status(IStatus.ERROR, LTK.PLUGIN_ID, -1,
+					"An error occured while notifying an ElementChangedListener.", exception));  //$NON-NLS-1$
+		}
+		
+	}
+	
+	protected class Task {
+		
+		private final ElementType fElement;
+		private InfoType fOldInfo;
+		private InfoType fNewInfo;
+		
+		
+		public Task(final ElementType element) {
+			fElement = element;
+		}
+		
+		
+		public ElementType getElement() {
+			return fElement;
+		}
+		
+		public InfoType getOldInfo() {
+			return fOldInfo;
+		}
+		
+		public InfoType getNewInfo() {
+			return fNewInfo;
+		}
+		
+		
+		void run() {
+			final IModelElementDelta delta = createDelta(this);
+			fireDelta(delta);
+		}
+	}
+	
+	
+	private final AbstractModelManager fModelManager;
+	
+	private final Object fTasksLock = new Object();
+	private final LinkedList<IModelElement> fTaskQueue = new LinkedList<IModelElement>();
+	private final HashMap<IModelElement, Task> fTaskDetail = new HashMap<IModelElement, Task>();
+	
+	private boolean fWorking = false;
+	private boolean fStop = false;
+	
+	
+	public AbstractModelEventJob(final AbstractModelManager manager) {
+		super("Model Events for " + manager.getModelTypeId()); //$NON-NLS-1$
+		setPriority(BUILD);
+		setSystem(true);
+		setUser(false);
+		
+		fModelManager = manager;
+	}
+	
+	
+	public void addUpdate(final ElementType element,
+			final InfoType oldModel, final InfoType newModel) {
+		synchronized (fTasksLock) {
+			Task task = fTaskDetail.get(element);
+			if (task == null) {
+				task = new Task(element);
+				task.fOldInfo = oldModel;
+				fTaskDetail.put(element, task);
+			}
+			else {
+				fTaskQueue.remove(element);
+			}
+			task.fNewInfo = newModel;
+			fTaskQueue.add(element);
+			
+			if (!fWorking) {
+				schedule();
+			}
+		}
+	}
+	
+	protected abstract IModelElementDelta createDelta(Task task);
+	
+	
+	@Override
+	protected IStatus run(final IProgressMonitor monitor) {
+		while (true) {
+			Task task;
+			synchronized (fTasksLock) {
+				final IModelElement element = (!fTaskQueue.isEmpty()) ? fTaskQueue.removeFirst() : null;
+				if (element == null || fStop) {
+					fWorking = false;
+					return Status.OK_STATUS;
+				}
+				fWorking = true;
+				task = fTaskDetail.remove(element);
+			}
+			try {
+				task.run();
+			}
+			catch (final Throwable e) {
+				LTKCorePlugin.getDefault().log(new Status(IStatus.ERROR, LTK.PLUGIN_ID, -1,
+						"An error occurred when firing model event for " + fModelManager.getModelTypeId() + ".", e )); //$NON-NLS-1$
+			}
+		}
+	}
+	
+	protected void dispose() {
+		synchronized (fTasksLock) {
+			fStop = true;
+			fTaskQueue.clear();
+			fTaskDetail.clear();
+		}
+	}
+	
+	private void fireDelta(final IModelElementDelta delta) {
+		final ISourceUnit su = LTKUtil.getSourceUnit(delta.getModelElement());
+		if (su == null) {
+			return;
+		}
+		final WorkingContext context = su.getWorkingContext();
+		final ElementChangedEvent event = new ElementChangedEvent(delta, context);
+		final SafeRunnable runnable = new SafeRunnable(event);
+		final IElementChangedListener[] listeners = fModelManager.getElementChangedListeners(context);
+		for (int i = 0; i < listeners.length; i++) {
+			runnable.fListener = listeners[i];
+			SafeRunner.run(runnable);
+		}
+	}
+	
+}
---
+++ de.walware.ecommons.ltk.core/src/de/walware/ecommons/ltk/core/impl/AbstractModelManager.java
@@ -0,0 +1,176 @@
+/*******************************************************************************
+ * Copyright (c) 2009-2010 WalWare/StatET-Project (www.walware.de/goto/statet)
+ * and others. All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Stephan Wahlbrink - initial API and implementation
+ *******************************************************************************/
+
+package de.walware.ecommons.ltk.core.impl;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+
+import de.walware.ecommons.FastList;
+
+import de.walware.ecommons.ltk.IElementChangedListener;
+import de.walware.ecommons.ltk.IModelManager;
+import de.walware.ecommons.ltk.ISourceUnit;
+import de.walware.ecommons.ltk.LTK;
+import de.walware.ecommons.ltk.WorkingContext;
+
+
+public abstract class AbstractModelManager implements IModelManager {
+	
+	
+	protected static class ContextItem {
+		
+		public final WorkingContext context;
+		
+		public final FastList<IElementChangedListener> listeners = new FastList<IElementChangedListener>(IElementChangedListener.class);
+		
+		
+		protected ContextItem(final WorkingContext context) {
+			this.context = context;
+		}
+		
+		
+		@Override
+		public final int hashCode() {
+			return this.context.hashCode();
+		}
+		
+		@Override
+		public final boolean equals(final Object obj) {
+			return (this == obj);
+		}
+		
+	}
+	
+	private static final IElementChangedListener[] NO_LISTENERS = new IElementChangedListener[0];
+	
+	private class RefreshJob extends Job {
+		
+		
+		private final List<ISourceUnit> fList;
+		
+		
+		public RefreshJob(final WorkingContext context) {
+			super("Model Refresh"); //$NON-NLS-1$
+			setUser(false);
+			setSystem(true);
+			setPriority(DECORATE);
+			
+			fList = LTK.getSourceUnitManager().getOpenSourceUnits(fTypeId, context);
+		}
+		
+		@Override
+		protected IStatus run(final IProgressMonitor monitor) {
+			for (final ISourceUnit su : fList) {
+				reconcile(su, (IModelManager.MODEL_FILE | IModelManager.RECONCILER), monitor);
+			}
+			return Status.OK_STATUS;
+		}
+		
+	}
+	
+	
+	
+	private final String fTypeId;
+	
+	private final FastList<ContextItem> fContexts = new FastList<ContextItem>(ContextItem.class, FastList.IDENTITY);
+	
+	
+	public AbstractModelManager(final String typeId) {
+		fTypeId = typeId;
+	}
+	
+	
+	public String getModelTypeId() {
+		return fTypeId;
+	}
+	
+	protected ContextItem getContextItem(final WorkingContext context, final boolean create) {
+		while (true) {
+			final ContextItem[] contextItems = fContexts.toArray();
+			for (int i = 0; i < contextItems.length; i++) {
+				if (contextItems[i].context == context) {
+					return contextItems[i];
+				}
+			}
+			if (!create) {
+				return null;
+			}
+			synchronized (fContexts) {
+				if (contextItems == fContexts.toArray()) {
+					final ContextItem item = doCreateContextItem(context);
+					fContexts.add(item);
+					return item;
+				}
+			}
+		}
+	}
+	
+	protected ContextItem doCreateContextItem(final WorkingContext context) {
+		return new ContextItem(context);
+	}
+	
+	protected ContextItem getExistingContext(final WorkingContext context) {
+		final ContextItem[] contextItems = fContexts.toArray();
+		for (int i = 0; i < contextItems.length; i++) {
+			if (contextItems[i].context == context) {
+				return contextItems[i];
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public void addElementChangedListener(final IElementChangedListener listener, final WorkingContext context) {
+		final ContextItem contextItem = getContextItem(context, true);
+		contextItem.listeners.add(listener);
+	}
+	
+	@Override
+	public void removeElementChangedListener(final IElementChangedListener listener, final WorkingContext context) {
+		final ContextItem contextItem = getContextItem(context, false);
+		if (contextItem != null) {
+			contextItem.listeners.remove(listener);
+		}
+	}
+	
+	protected IElementChangedListener[] getElementChangedListeners(final WorkingContext context) {
+		final ContextItem contextItem = getExistingContext(context);
+		if (context == null) {
+			return NO_LISTENERS;
+		}
+		return contextItem.listeners.toArray();
+	}
+	
+	/**
+	 * Refresh reuses existing ast
+	 */
+	@Override
+	public void refresh(final WorkingContext context) {
+		new RefreshJob(context).schedule();
+	}
+	
+	@Override
+	public void registerDependentUnit(final ISourceUnit su) {
+	}
+	
+	@Override
+	public void deregisterDependentUnit(final ISourceUnit su) {
+	}
+	
+	
+	protected abstract void reconcile(ISourceUnit su, int level, IProgressMonitor monitor);
+	
+}
