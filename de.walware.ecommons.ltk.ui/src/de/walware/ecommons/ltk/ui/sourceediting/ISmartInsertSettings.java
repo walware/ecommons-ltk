6ad6c133b3bb0c26f9aea0c3767f57efb5a1555a
Stephan Wahlbrink <sw@wahlbrink.eu>
2012-01-21 22:47:48 +0100

    [LTK-Editor] Added tools for smart insert
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

---
+++ de.walware.ecommons.ltk.ui/src/de/walware/ecommons/ltk/ui/sourceediting/ISmartInsertSettings.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2012 WalWare/StatET-Project (www.walware.de/goto/statet).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Stephan Wahlbrink - initial API and implementation
+ *******************************************************************************/
+
+package de.walware.ecommons.ltk.ui.sourceediting;
+
+
+public interface ISmartInsertSettings {
+	
+	
+	enum TabAction {
+		INSERT_TAB_CHAR,
+		INSERT_TAB_LEVEL,
+		INSERT_INDENT_LEVEL,
+		CORRECT_INDENT,
+	}
+	
+}
---
+++ de.walware.ecommons.ltk.ui/src/de/walware/ecommons/ltk/ui/sourceediting/SmartInsertSettingsUI.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2012 WalWare/StatET-Project (www.walware.de/goto/statet).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Stephan Wahlbrink - initial API and implementation
+ *******************************************************************************/
+
+package de.walware.ecommons.ltk.ui.sourceediting;
+
+import org.eclipse.jface.viewers.LabelProvider;
+
+
+public class SmartInsertSettingsUI {
+	
+	
+	public static class SettingsLabelProvider extends LabelProvider {
+		
+		@Override
+		public String getText(final Object element) {
+			if (element instanceof ISmartInsertSettings.TabAction) {
+				switch ((ISmartInsertSettings.TabAction) element) {
+				case INSERT_TAB_CHAR:
+					return "Insert Tab Char";
+				case INSERT_INDENT_LEVEL:
+					return "Insert Indent Level";
+				case CORRECT_INDENT:
+					return "Adjust Indentation";
+				}
+			}
+			return super.getText(element);
+		}
+		
+	}
+	
+}
